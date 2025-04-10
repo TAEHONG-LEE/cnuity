b9fb6fee57d8336b8fb48ddad1904cbae2eb8f96
TAEHONG-LEE <2t_hong@naver.com>
2025-04-10 16:18:22 +0900

    Add: 정사각형 서비스 버튼 ServiceSquareButton 위젯 컴포넌트화

---
+++ lib/widgets/common/service_square_button.dart
@@ -0,0 +1,43 @@
+// lib/widgets/common/service_square_button.dart
+import 'package:flutter/material.dart';
+
+class ServiceSquareButton extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const ServiceSquareButton({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF5197FF),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 50, color: Colors.white),
+            const SizedBox(height: 12),
+            Text(
+              label,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
